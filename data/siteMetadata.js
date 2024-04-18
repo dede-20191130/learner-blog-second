6b5a13631e9104ffe56ef0dfe96ff05a8c3e870d
dede_20191130 <1044adad@gmail.com>
2024-04-19 08:32:28 +0900

    add GA id

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -39,7 +39,7 @@ const siteMetadata = {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: '', // e.g. G-XXXXXXX //todo
+      googleAnalyticsId: 'G-PK0YGFR62Q', // e.g. G-XXXXXXX
     },
   },
   // newsletter: {
