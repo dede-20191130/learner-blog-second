45e6c26de6bb50c5c6455f0120c054164d54340b
dede_20191130 <1044adad@gmail.com>
2024-04-19 09:18:06 +0900

    setup giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -48,7 +48,6 @@ const siteMetadata = {
   //   provider: 'buttondown',
   // },
   comments: {
-    //todo
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
